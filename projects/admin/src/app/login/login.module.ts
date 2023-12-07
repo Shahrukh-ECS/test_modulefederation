07313cb30900792c878fa4d0b9ef1797be5ab031
Muhamad Shahrukh Khan <shahrukh.khan@eazio.com>
2023-12-07 19:09:20 +0500

    first commit

---
+++ projects/admin/src/app/login/login.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { SignupComponent } from './signup/signup.component';
+
+
+@NgModule({
+  declarations: [
+    SignupComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }
---
+++ projects/admin/webpack.config.js
@@ -0,0 +1,19 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  name: "admin",
+  exposes: {
+    "./Module": "./projects/admin/src/app/login/login.module.ts",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
